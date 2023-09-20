568aee3c1f2dd479262eee027f927b7c5cb8a3bf
Mukesh <mackrathod014@gmail.com>
2023-09-20 16:17:47 +0530

    created Header  component

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// ************ Importing Routes ****************************
 import Home from './pages/Home';
 import About from './pages/About';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Profile from './pages/Profile';
+import Header from './components/Header';
+
+
+// *********** below  is the code **************************
 
 
 
@@ -13,6 +19,7 @@ const App = () => {
   return (
 
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
